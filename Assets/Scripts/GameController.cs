44b9aec451e70ab09b9aa057b30353b033398a06
nimp0 <cloud_nimpo@ukr.net>
2018-08-21 20:11:40 +0300

    initial commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] pictures = new GameObject[15];
+    private List<Transform> sprites = new List<Transform>();
+    public bool isRunning;
+    public float timeRunning;
+    public float speed;
+
+    public void Awake()
+    {
+        for (int i = 0; i < pictures.Length; i++)
+        {
+            sprites.Add(pictures[i].transform);
+        }
+    }
+
+    public void StartMoveCoroutine()
+    {
+        if (isRunning)
+        {
+            return;
+        }
+
+        else
+        {
+
+            StartCoroutine("Movement");
+        }
+    }
+
+    public IEnumerator Movement()
+    {
+        StartCoroutine("LeftMove");
+        yield return new WaitForSeconds(1f);
+        StartCoroutine("MidMove");
+        yield return new WaitForSeconds(1f);
+        StartCoroutine("RightMove");
+        yield return new WaitForSeconds(1f);
+    }
+    
+    public IEnumerator LeftMove()
+    {
+        speed = 1f;
+        isRunning = true;
+        timeRunning = 40f;
+
+        float deltaTime = Time.deltaTime * speed;
+
+        while (timeRunning > 0)
+        {
+            foreach (int index in Enumerable.Range(5, 5))
+            {
+                Vector2 startPos = new Vector2(sprites[index].transform.position.x, sprites[index].transform.position.y);
+                Vector2 endPos = new Vector2(sprites[index].transform.position.x, -20);
+                float distance = Vector2.Distance(startPos, endPos);
+                //float distance = -movementVector.y;
+                //float currTimeRunning = timeRunning - deltaTime * 0.01f;
+                //float currDistance = distance - (deltaTime * 0.01f);
+                //t = currTimeRunning * deltaTime / currDistance;
+                float t = timeRunning * deltaTime / distance;
+                sprites[index].transform.position = Vector2.Lerp(startPos, endPos, t);
+                //sprites[index].transform.Translate(movementVector * deltaTime * t);
+            }
+            Teleport();
+            timeRunning -= deltaTime;
+            yield return new WaitForSeconds(deltaTime * 0.01f);
+        }
+        //isRunning = false;
+        RoundUpThePositionValue();
+    }
+
+    public IEnumerator MidMove()
+    {
+        speed = 1f;
+        //isRunning = true;
+        timeRunning = 40f;
+
+        float deltaTime = Time.deltaTime * speed;
+
+        while (timeRunning > 0)
+        {
+            foreach (int index in Enumerable.Range(0, 5))
+            {
+                Vector2 startPos = new Vector2(sprites[index].transform.position.x, sprites[index].transform.position.y);
+                Vector2 endPos = new Vector2(sprites[index].transform.position.x, -20);
+                float distance = Vector2.Distance(startPos, endPos);
+                //float distance = -movementVector.y;
+                //float currTimeRunning = timeRunning - deltaTime * 0.01f;
+                //float currDistance = distance - (deltaTime * 0.01f);
+                //t = currTimeRunning * deltaTime / currDistance;
+                float t = timeRunning * deltaTime / distance;
+                sprites[index].transform.position = Vector2.Lerp(startPos, endPos, t);
+                //sprites[index].transform.Translate(movementVector * deltaTime * t);
+            }
+            Teleport();
+            timeRunning -= deltaTime;
+            yield return new WaitForSeconds(deltaTime * 0.01f);
+        }
+        //isRunning = false;
+        RoundUpThePositionValue();
+
+    }
+
+    public IEnumerator RightMove()
+    {
+        speed = 1f;
+        //isRunning = true;
+        timeRunning = 40f;
+
+        float deltaTime = Time.deltaTime * speed;
+
+        while (timeRunning > 0)
+        {
+            foreach (int index in Enumerable.Range(10, 5))
+            {
+                Vector2 startPos = new Vector2(sprites[index].transform.position.x, sprites[index].transform.position.y);
+                Vector2 endPos = new Vector2(sprites[index].transform.position.x, -20);
+                float distance = Vector2.Distance(startPos, endPos);
+                //float distance = -movementVector.y;
+                //float currTimeRunning = timeRunning - deltaTime * 0.01f;
+                //float currDistance = distance - (deltaTime * 0.01f);
+                //t = currTimeRunning * deltaTime / currDistance;
+                float t = timeRunning * deltaTime / distance;
+                sprites[index].transform.position = Vector2.Lerp(startPos, endPos, t);
+                //sprites[index].transform.Translate(movementVector * deltaTime * t);
+            }
+            Teleport();
+            timeRunning -= deltaTime;
+            yield return new WaitForSeconds(deltaTime * 0.01f);
+        }
+        isRunning = false;
+        RoundUpThePositionValue();
+    }
+
+    public void Teleport()
+    {
+        for (int j = 0; j < sprites.Count; j++)
+        {
+            if (sprites[j].transform.position.y <= -6f)
+            {
+                Vector2 currentPosition = sprites[j].transform.position;
+                currentPosition.y = (currentPosition.y + 10f);
+                sprites[j].transform.position = currentPosition;
+            }
+        }
+    }
+
+    public void RoundUpThePositionValue()
+    {
+        for (int j = 0; j < sprites.Count; j++)
+        {
+            float missalignedY = sprites[j].transform.position.y;
+            float alignedY = Mathf.RoundToInt(missalignedY);
+            if (alignedY % 2 != 0)
+            {
+                sprites[j].transform.position = new Vector3(sprites[j].transform.position.x, alignedY - 1, sprites[j].transform.position.z);
+            }
+
+            else
+            {
+                sprites[j].transform.position = new Vector3(sprites[j].transform.position.x, alignedY, sprites[j].transform.position.z);
+            }
+        }
+    }
+}
+
+
+
