c2364cac108a8dfa8ce1acf5b28e465d4c33b030
nimp0 <cloud_nimpo@ukr.net>
2018-08-26 19:12:43 +0300

    added random probability of end positions of y

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
+public class EndPositions<T>
+{
+    public double Probability { get; set; }
+    public T EndPosition { get; set; }
+}
+
 public class GameController : MonoBehaviour
 {
     public GameObject[] pictures = new GameObject[15];
@@ -10,6 +16,34 @@ public class GameController : MonoBehaviour
     private float[] startPositionsOfSpritesY = new float[15] { 0, -2, -4, -6, -8, 0, -2, -4, -6, -8, 0, -2, -4, -6, -8 };
     public bool isRunning;
     public float t;
+    public float yEndPosition;
+
+    public EndPositions<float> GetRandomValueOfYEndPos()
+    {
+        var initial = new List<EndPositions<float>>
+        {
+            new EndPositions<float> {Probability = 30 / 100.0, EndPosition = -100},
+            new EndPositions<float> {Probability = 25 / 100.0, EndPosition = -102},
+            new EndPositions<float> {Probability = 20 / 100.0, EndPosition = -104},
+            new EndPositions<float> {Probability = 15 / 100.0, EndPosition = -106},
+            new EndPositions<float> {Probability = 10 / 100.0, EndPosition = -108},
+        };
+
+        var converted = new List<EndPositions<float>>(initial.Count);
+        var sum = 0.0;
+        foreach (var item in initial.Take(initial.Count - 1))
+        {
+            sum += item.Probability;
+            converted.Add(new EndPositions<float> { Probability = sum, EndPosition = item.EndPosition });
+        }
+        converted.Add(new EndPositions<float> { Probability = 1.0, EndPosition = initial.Last().EndPosition });
+
+        var random = new System.Random();
+        var probability = random.NextDouble();
+        var selected = converted.SkipWhile(i => i.Probability < probability).First();
+
+        return selected;
+    }
 
     public void Awake()
     {
@@ -28,44 +62,42 @@ public class GameController : MonoBehaviour
 
         else
         {
+            yEndPosition = GetRandomValueOfYEndPos().EndPosition;
             StartCoroutine("Movement");
         }
     }
 
     public IEnumerator Movement()
     {
+        t = 0;
         StartCoroutine(MoveReels(0));
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(1);
         StartCoroutine(MoveReels(1));
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(1);
         StartCoroutine(MoveReels(2));
-        yield return new WaitForSeconds(1f);
     }
 
     public IEnumerator MoveReels(int order)
     {
         isRunning = true;
-        t = 0;
-
         while (t < 1)
         {
+            float delta = TimeDeltaAsRandom();
             float c = 0.5f;
             if (t > c)
             {
-                float t1 = Mathf.Sqrt((1 - t) * (1 / c)) * Time.deltaTime / 10;
+                float t1 = Mathf.Sqrt((1 - t) * (1 / c)) * delta / 10;
                 t += t1;
             }
             else
             {
-                t += Time.deltaTime / 10;
+                t += delta / 10;
             }
-
             foreach (int index in Enumerable.Range(order * 5, 5))
             {
-                Vector2 startPos = new Vector2(sprites[index].position.x, startPositionsOfSpritesY[index]);
-                Vector2 endPos = new Vector2(sprites[index].position.x, startPositionsOfSpritesY[index] - 100);
+                Vector2 startPos = new Vector2(sprites[index].transform.position.x, startPositionsOfSpritesY[index]);
+                Vector2 endPos = new Vector2(sprites[index].transform.position.x, startPositionsOfSpritesY[index] + yEndPosition);
                 sprites[index].transform.position = Vector2.Lerp(startPos, endPos, t);
-
                 Vector2 fakePos = sprites[index].transform.position;
                 fakePos.y %= 10;
                 sprites[index].transform.position = fakePos;
@@ -74,6 +106,11 @@ public class GameController : MonoBehaviour
         }
         isRunning = false;
         RoundUpThePositionValue();
+    }
+
+    private float TimeDeltaAsRandom()
+    {
+        return Random.Range(0.0111f, 0.01f);
     }
 
     public void RoundUpThePositionValue()
