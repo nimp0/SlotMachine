d4dc222d1e9c63cf95ce998348dd65851e4ab423
nimp0 <cloud_nimpo@ukr.net>
2018-08-29 12:33:01 +0300

    final version of movement

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,73 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
-
-public class EndPositions<T>
-{
-    public double Probability { get; set; }
-    public T EndPosition { get; set; }
-}
 
 public class GameController : MonoBehaviour
 {
-    public GameObject[] picturesL = new GameObject[5];
-    public GameObject[] picturesM = new GameObject[5];
-    public GameObject[] picturesR = new GameObject[5];
+    public List<List<Transform>> sprites;
+    public float[] tParameters;
     public bool isRunning;
-    public float t1;
-    public float t2;
-    public float t3;
-    public float yEndPositionLeft;
-    public float yEndPositionMid;
-    public float yEndPositionRight;
 
-    private List<Transform> spritesLeft = new List<Transform>();
-    private List<Transform> spritesMid = new List<Transform>();
-    private List<Transform> spritesRight = new List<Transform>();
-    private float[] yStartPosition = new float[5] { 0, -2, -4, -6, -8 };
+    public int multiplier = 20;
+    int amountOfRows = 3;
+    int amountOfSprites = 15;
+    int amountOfLoadedSprites = 5;
+    int[][] probabilities;
 
-    public EndPositions<float> GetRandomValueOfYEndPos()
-    {
-        var initial = new List<EndPositions<float>>
-        {
-            new EndPositions<float> {Probability = 30 / 100.0, EndPosition = -100},
-            new EndPositions<float> {Probability = 20 / 100.0, EndPosition = -102},
-            new EndPositions<float> {Probability = 10 / 100.0, EndPosition = -104},
-            new EndPositions<float> {Probability = 15 / 100.0, EndPosition = -106},
-            new EndPositions<float> {Probability = 25 / 100.0, EndPosition = -108},
-        };
-
-        var converted = new List<EndPositions<float>>(initial.Count);
-        var sum = 0.0;
-        foreach (var item in initial.Take(initial.Count - 1))
-        {
-            sum += item.Probability;
-            converted.Add(new EndPositions<float> { Probability = sum, EndPosition = item.EndPosition });
-        }
-        converted.Add(new EndPositions<float> { Probability = 1.0, EndPosition = initial.Last().EndPosition });
-
-        var random = new System.Random();
-        var probability = random.NextDouble();
-        var selected = converted.SkipWhile(i => i.Probability < probability).First();
-
-        return selected;
-    }
+    private float[] yStartPositions;
+    private float yEndPosition;
 
     public void Awake()
     {
-        for (int l = 0; l < picturesL.Length; l++)
+        amountOfSprites *= multiplier;
+        amountOfRows *= multiplier;
+        probabilities = new int[amountOfRows][];
+
+        for (int i = 0; i < amountOfRows; i++)
         {
-            spritesLeft.Add(picturesL[l].transform);
+            probabilities[i] = new int[amountOfLoadedSprites];
         }
-        for (int m = 0; m < picturesM.Length; m++)
+
+        for (int i = 0; i < amountOfRows; i++)
         {
-            spritesMid.Add(picturesM[m].transform);
+            probabilities[i][0] = 1 * multiplier;
+            probabilities[i][1] = 2 * multiplier;
+            probabilities[i][2] = 3 * multiplier;
+            probabilities[i][3] = 4 * multiplier;
+            probabilities[i][4] = 5 * multiplier;
         }
-        for (int r = 0; r < picturesR.Length; r++)
+
+        yStartPositions = new float[amountOfSprites];
+
+        for(int i = 0; i < amountOfRows; i++)
         {
-            spritesRight.Add(picturesR[r].transform);
+            tParameters = new float[amountOfRows];
         }
+
+        SetSprites();
     }
 
     public void StartMoveCoroutine()
@@ -85,160 +62,138 @@ public class GameController : MonoBehaviour
 
     public IEnumerator Movement()
     {
-        t1 = 0;
-        StartCoroutine("MoveReelsLeft");
-        yield return new WaitForSeconds(1);
-        t2 = 0;
-        StartCoroutine("MoveReelsMid");
-        yield return new WaitForSeconds(1);
-        t3 = 0;
-        StartCoroutine("MoveReelsRight");
+        for (int i = 0; i < amountOfRows; i++)
+        {
+            tParameters[i] = 0;
+            StartCoroutine(MoveReelByIndex(i));
+            yield return new WaitForSeconds(.2f);
+        }
     }
 
-    /*public float CalculateT(float t)
+    public IEnumerator MoveReelByIndex(int index)
     {
-        float delta = TimeDeltaAsRandom();
-        float c = 0.5f;
-        if (t > c)
+        isRunning = true;
+
+        List<Transform> row = sprites[index];
+        float yEndPosition = -(Random.Range(0, amountOfSprites) * 2 + amountOfSprites * 20);
+        float t = tParameters[index];
+
+        while (t < 1)
         {
-            float tNext = Mathf.Sqrt((1 - t1) * (1 / c)) * delta/10;
-            t += tNext;
+            float speed = .5f;
+            float delta = 0.01f;
+            float c1 = 0.5f;
+
+            if (t > c1)
+            {
+                float tNext = Mathf.Sqrt((1 - t) * (1 / c1)) * delta * speed;
+                t += tNext;
+            }
+            else if (t < c1)
+            {
+                float tNext = Mathf.Sqrt((.01f + t) * (1 / c1)) * delta * speed * speed;
+                t += tNext;
+            }
+            else
+            {
+                t += delta * speed;
+            }
+
+            if (t > 1)
+            {
+                t = 1;
+            }
+
+            for (int i = 0; i < row.Count; i++)
+            {
+                Vector3 startPos = new Vector3(row[i].position.x, yStartPositions[i], row[i].position.z);
+                Vector3 endPos = new Vector3(row[i].position.x, yStartPositions[i] + yEndPosition, row[i].position.z);
+                row[i].position = Vector3.Lerp(startPos, endPos, t);
+                Vector3 fakePos = row[i].position;
+                fakePos.y %= amountOfSprites * 2;
+                row[i].position = fakePos;
+            }
+            tParameters[index] = t;
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        tParameters[index] = t;
+        isRunning = false;
+        //RoundUpThePositionValueOfRows();
+    }
+
+    void RoundUpThePositionValueOfRows()
+    {
+        for (int i = 0; i < sprites.Count; i++)
+        {
+            List<Transform> row = sprites[i];
+
+            for (int j = 0; j < row.Count; j++)
+            {
+                float missalignedY = row[j].position.y;
+                float alignedY = Mathf.RoundToInt(missalignedY);
+                alignedY += alignedY % 2;
+                row[j].position = new Vector3(row[j].position.x, alignedY, row[j].position.z);
+            }
+        }
+    }
+
+    void SetSprites()
+    {
+        List<GameObject> spritesFromResources = new List<GameObject>();
+
+        for (int i = 0; i < amountOfLoadedSprites; i++)
+        {
+            GameObject loadedSprite = Resources.Load("Prefabs/" + i.ToString()) as GameObject;
+            spritesFromResources.Add(loadedSprite);
+        }
+
+        sprites = new List<List<Transform>>();
+
+        for (int i = 0; i < amountOfRows; i++)
+        {
+            List<Transform> row = new List<Transform>();
+
+            for (int j = 0; j < amountOfSprites; j++)
+            {
+                int r;
+
+                do
+                {
+                    r = Random.Range(0, amountOfLoadedSprites);
+                }
+                while (!CheckIfIndexIsAllowedForAdding(r, i));
+
+                Transform spriteInstanceInScene = Instantiate(spritesFromResources[r]).transform;
+                row.Add(spriteInstanceInScene);
+            }
+
+            sprites.Add(row);
+        }
+
+        for (int i = 0; i < amountOfRows; i++)
+        {
+            List<Transform> row = sprites[i];
+
+            for (int j = 0; j < amountOfSprites; j++)
+            {
+                Transform sprite = row[j];
+                sprite.position = new Vector3(i * 2, -j * 2, 10);
+                yStartPositions[j] = sprite.position.y;
+            }
+        }
+    }
+
+    bool CheckIfIndexIsAllowedForAdding(int index, int rowIndex)
+    {
+        if (probabilities[rowIndex][index] == 0)
+        {
+            return false;
         }
         else
         {
-            t += delta/10;
-        }
-        return t;
-    }*/
-    public IEnumerator MoveReelsLeft()
-    {
-        yEndPositionLeft = GetRandomValueOfYEndPos().EndPosition;
-        isRunning = true;
-
-        while (t1 < 1)
-        {
-            float delta = TimeDeltaAsRandom();
-            float c = 0.5f;
-            if (t1 > c)
-            {
-                float tNext1 = Mathf.Sqrt((1 - t1) * (1 / c)) * delta / 2;
-                t1 += tNext1;
-            }
-            else
-            {
-                t1 += delta / 2;
-            }
-            for (int l = 0; l < spritesLeft.Count; l++)
-            {
-                Vector3 startPos = new Vector3(spritesLeft[l].transform.position.x, yStartPosition[l], spritesLeft[l].transform.position.z);
-                Vector3 endPos = new Vector3(spritesLeft[l].transform.position.x, yStartPosition[l] + yEndPositionLeft, spritesLeft[l].transform.position.z);
-                spritesLeft[l].transform.position = Vector3.Lerp(startPos, endPos, t1);
-                Vector3 fakePos = spritesLeft[l].transform.position;
-                fakePos.y %= 10;
-                spritesLeft[l].transform.position = fakePos;
-            }
-            yield return new WaitForSeconds(0.01f);
-        }
-        isRunning = false;
-        RoundUpThePositionValueOfLeft();
-    }
-
-    public IEnumerator MoveReelsMid()
-    {
-        yEndPositionMid = GetRandomValueOfYEndPos().EndPosition;
-        isRunning = true;
-        while (t2 < 1)
-        {
-            float delta = TimeDeltaAsRandom();
-            float c = 0.5f;
-            if (t2 > c)
-            {
-                float tNext = Mathf.Sqrt((1 - t2) * (1 / c)) * delta / 2;
-                t2 += tNext;
-            }
-            else
-            {
-                t2 += delta / 2;
-            }
-            for (int m = 0; m < spritesMid.Count; m++)
-            {
-                Vector3 startPos = new Vector3(spritesMid[m].transform.position.x, yStartPosition[m], spritesMid[m].transform.position.z);
-                Vector3 endPos = new Vector3(spritesMid[m].transform.position.x, yStartPosition[m] + yEndPositionMid, spritesMid[m].transform.position.z);
-                spritesMid[m].transform.position = Vector3.Lerp(startPos, endPos, t2);
-                Vector3 fakePos = spritesMid[m].transform.position;
-                fakePos.y %= 10;
-                spritesMid[m].transform.position = fakePos;
-            }
-            yield return new WaitForSeconds(0.01f);
-        }
-        isRunning = false;
-        RoundUpThePositionValueOfMid();
-    }
-
-    public IEnumerator MoveReelsRight()
-    {
-        yEndPositionRight = GetRandomValueOfYEndPos().EndPosition;
-        isRunning = true;
-        while (t3 < 1)
-        {
-            float delta = TimeDeltaAsRandom();
-            float c = 0.5f;
-            if (t3 > c)
-            {
-                float tNext = Mathf.Sqrt((1 - t3) * (1 / c)) * delta / 2;
-                t3 += tNext;
-            }
-            else
-            {
-                t3+= delta / 2;
-            }
-            for (int r = 0; r < spritesRight.Count; r++)
-            {
-                Vector3 startPos = new Vector3(spritesRight[r].transform.position.x, yStartPosition[r], spritesRight[r].transform.position.z);
-                Vector3 endPos = new Vector3(spritesRight[r].transform.position.x, yStartPosition[r] + yEndPositionRight, spritesRight[r].transform.position.z);
-                spritesRight[r].transform.position = Vector3.Lerp(startPos, endPos, t3);
-                Vector3 fakePos = spritesRight[r].transform.position;
-                fakePos.y %= 10;
-                spritesRight[r].transform.position = fakePos;
-            }
-            yield return new WaitForSeconds(0.01f);
-        }
-        isRunning = false;
-        RoundUpThePositionValueOfRight();
-    }
-
-    private float TimeDeltaAsRandom()
-    {
-        return Random.Range(0.0111f, 0.01f);
-    }
-
-    public void RoundUpThePositionValueOfLeft()
-    {
-        for (int l = 0; l < spritesLeft.Count; l++)
-        {
-            float missalignedY = spritesLeft[l].transform.position.y;
-            float alignedY = Mathf.RoundToInt(missalignedY);
-            spritesLeft[l].transform.position = new Vector3(spritesLeft[l].transform.position.x, alignedY, spritesLeft[l].transform.position.z);
-        }
-    }
-
-    public void RoundUpThePositionValueOfMid()
-    {
-        for (int m = 0; m < spritesMid.Count; m++)
-        {
-            float missalignedY = spritesMid[m].transform.position.y;
-            float alignedY = Mathf.RoundToInt(missalignedY);
-            spritesMid[m].transform.position = new Vector3(spritesMid[m].transform.position.x, alignedY, spritesMid[m].transform.position.z);
-        }
-    }
-
-    public void RoundUpThePositionValueOfRight()
-    {
-        for (int r = 0; r < spritesRight.Count; r++)
-        {
-            float missalignedY = spritesRight[r].transform.position.y;
-            float alignedY = Mathf.RoundToInt(missalignedY);
-            spritesRight[r].transform.position = new Vector3(spritesRight[r].transform.position.x, alignedY, spritesRight[r].transform.position.z);
+            probabilities[rowIndex][index]--;
+            return true;
         }
     }
 }
